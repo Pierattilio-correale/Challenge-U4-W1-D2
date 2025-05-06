8bc30fddf9ca38d2f1eb5f574ebd35e3d5d71f8d
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-05-06 15:47:59 +0200

    adds scanner on ex1

--- src/es1/Esercizio1.java
+++ src/es1/Esercizio1.java
@@ -1,22 +1,29 @@
 package es1;
 
+import java.util.Scanner;
+
 public class Esercizio1 {
     public static void main(String[] args) {
 //esercizio paridispari
-       boolean ciao= stringaPariDispari("ciaone");
+       boolean ciao= stringaPariDispari();
         System.out.println(ciao);
 
         // esercizio data
-        boolean data = annoBisestile(1900);
+        boolean data = annoBisestile();
         System.out.println(data);
-        boolean data2 = annoBisestile(2028);
-        System.out.println(data2);
+
     }
-    public static boolean stringaPariDispari(String str){
+    public static boolean stringaPariDispari(){
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("inserisci la stringa ");
+        String str= scanner2.nextLine();
       int c=  str.length();
         return str.length() % 2 == 0;
     }
-    public static boolean annoBisestile(int anno){
+    public static boolean annoBisestile(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci la data per verificare che sia bisestile o meno");
+        int anno= scanner.nextInt();
        return  (anno % 4 == 0) && ((anno % 100 != 0) || (anno % 400 == 0));
     }
 }
